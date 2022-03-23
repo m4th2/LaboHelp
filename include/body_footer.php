b99b0e7121bdc980d59f580546fcbf679641495e
IkaRio198 <ikario198@gmail.com>
2022-03-23 21:42:22 +0100

    Compteur d'élèves - Ajout d'un "compteur" d'élèves :P - Correction de la ref 87

--- include/body_footer.php
+++ include/body_footer.php
@@ -2,5 +2,6 @@
 	<p class="small">
 	Une idée originale de Vincent ROBERT, développé avec Kevin FEDYNA, illustré par Alexandre GUTIERREZ, maintenu par Ilyas RAHMOUN
 	<br> Découvrez la <a href="https://nsi.xyz">spécialité NSI</a> (numérique et sciences informatiques) du Lycée Louis Pasteur
+	<?php echo '<br> Selon <span class="source">"tkt"</span>, laboHelp a déjà aidé 4 242 élèves !'?>
 	</p>
 </div>
