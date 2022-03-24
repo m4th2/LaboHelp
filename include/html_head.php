bf12d0e84614c63c9a17e728b36bd757a658f193
IkaRio198 <ikario198@gmail.com>
2022-03-24 22:07:48 +0100

    CSS Responsive - Amélioration du css responsive (affichage mobile)

--- include/html_head.php
+++ include/html_head.php
@@ -3,4 +3,4 @@
 	<link rel="stylesheet" href="css/<?php if (isset($_GET["style"]) && $_GET["style"] == "no") {echo "css disabled";}?>style.css">
 	<link rel="icon" href="LaboRobot.png">
 	<title>laboHelp</title>
-</head>			
+</head>
